67912965b494165adf03261c4daab020da8bdad1
suojianfei <suojianfei@bestseller.com.cn>
2022-06-10 14:29:00 +0800

    添加ROIC文档及相关地址信息

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # stock_msci_roic
 ROIC based on MSCI index
+
+
+## MSCI China Index Download
+https://www.msci.com/msci%E6%8C%87%E6%95%B0%E4%BF%A1%E6%81%AF
+
+## PDF -> TXT
+https://app.xunjiepdf.com/ocrpdf/
+
+## 巨潮API地址
+http://webapi.cninfo.com.cn/#/apiDoc
+
+## 巨潮API - header中，mcCode获取
+### 1. 打开 http://webapi.cninfo.com.cn/#/dataBrowse
+### 2. F12查看文件： json2csv.js
+### 3. indexCode，即mcCode
---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * MSCI China Index 计算并分析个股ROIC数据
+ * @author: FindFly
+ * @since: 2022-06-06
+ */
+
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
